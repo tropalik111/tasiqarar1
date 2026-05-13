a93115b965cfa7c8141a8aabc2fd0c58d9ee197f
tropalik11 <mohammedalshammari793@gmail.com>
2026-05-14 01:22:42 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Qarar from './Qarar.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Qarar />
+  </React.StrictMode>,
+)
